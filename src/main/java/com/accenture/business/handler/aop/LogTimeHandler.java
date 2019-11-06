91e86c9619c9b6183694b815d6e89da697d0779b
k-onbfans <49852494+k-onbfans@users.noreply.github.com>
2019-11-06 10:29:09 +0800

    add @Around for logTime

--- src/main/java/com/accenture/business/handler/aop/LogTimeHandler.java
+++ src/main/java/com/accenture/business/handler/aop/LogTimeHandler.java
@@ -1,9 +1,7 @@
 package com.accenture.business.handler.aop;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,8 +11,6 @@ import java.text.SimpleDateFormat;
 @Aspect
 @Component
 public class LogTimeHandler {
-
-    private long startTime;
 
 
     private static final Logger logger = LoggerFactory.getLogger(LogTimeHandler.class);
@@ -27,10 +23,18 @@ public class LogTimeHandler {
     @Before("logTime()")
     public void doBefore (){
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
         String str = df.format(startTime);
         logger.info("Service start time : {}",str);
 
+    }
+
+    @Around("logTime()")
+    public void doAround(ProceedingJoinPoint point) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        point.proceed();
+        long endTime = System.currentTimeMillis();
+        logger.info("Time cost : {}",endTime-startTime);
     }
 
     @After("logTime()")
@@ -39,7 +43,5 @@ public class LogTimeHandler {
         long endTime = System.currentTimeMillis();
         String str = df.format(endTime);
         logger.info("Service end time : {}",str);
-        Long timeCost = endTime - startTime;
-        logger.info("Time cost: {} ms",timeCost);
     }
 }
