8130424d9cd06c4ef6dd685f33e2dfd35f94ce4c
k-onbfans <49852494+k-onbfans@users.noreply.github.com>
2019-11-06 14:13:09 +0800

    delete useless file and rebuild the project's structure

--- src/main/java/com/accenture/business/service/HttpService.java
+++ src/main/java/com/accenture/business/service/HttpService.java
@@ -2,15 +2,9 @@ package com.accenture.business.service;
 
 import com.accenture.business.request.FindByFlightNumberReq;
 import com.accenture.business.request.FindByRouteReq;
-import com.accenture.business.response.FlightRes;
-import com.accenture.business.response.FlightReses;
 import com.accenture.business.bean.Port;
 
 public interface HttpService {
-
-    public FlightRes flightResFromFindByFlightNumberReq(String url, FindByFlightNumberReq request);
-
-    public FlightReses flightResesFromFindByRouteReq(String url, FindByRouteReq request);
 
     public String getFlightV1(String url);
 
